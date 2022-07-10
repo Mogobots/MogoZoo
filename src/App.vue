f0e80d479e84b7865856e145d9e1e60dfc5b29bc
Matias Guarna <matias.guarna@gmail.com>
2022-07-10 16:06:05 -0300

    Added play button

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import PlayButton from "./components/PlayButton.vue";
+
+const botonPlay = () => {
+  console.log("No hago nada");
+};
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
+  <PlayButton msg="asd" />
+
+  <button type="button" class="botoncitoPlay" @click="botonPlay()">
+    Play <img alt="Play Icon" id="btnplay" src="./assets/iconplay.png" />
+  </button>
 </template>
 
 <style>
@@ -17,5 +27,10 @@ import HelloWorld from './components/HelloWorld.vue'
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#btnplay {
+  width: 30px;
+  height: 30px;
 }
 </style>
---
+++ src/components/PlayButton.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  name: "PlayButton",
+};
+
+// const botonPlay = () => {
+//   console.log("No hago nada");
+// };
+</script>
+
+<template>
+  <div>
+    <h1>COMPONENTE BOTONCITO</h1>
+    <button type="button" class="botoncitoPlay" @click="botonPlay()">
+      Play <img alt="Play Icon" id="btnplay" src="./assets/iconplay.png" />
+    </button>
+  </div>
+</template>
+
+<style scoped>
+.botoncitoPlay {
+  color: orange;
+  border: solid 2px black;
+}
+
+#btnplay {
+  width: 30px;
+  height: 30px;
+}
+</style>
